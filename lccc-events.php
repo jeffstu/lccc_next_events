ef4a5fb357ee4fca7053c0e163639fe4582a25fd
jeffstu <stuartlowe@hotmail.co.uk>
2023-12-24 03:48:07 +0000

    Update lccc-events.php
    attempt to add some elementor functionality

--- lccc-events.php
+++ lccc-events.php
@@ -6,34 +6,36 @@ Version: 1.0
 Author: Stu Lowe
 */
 
-
 function display_lccc_next_events( $atts ) {
-    // Set up default parameters
+    // Shortcode default attributes
     $atts = shortcode_atts( array(
-        'events' => 3, // Default number of events if none provided
+        'events' => 3,
     ), $atts, 'lccc_next_events' );
 
-    // Fetch the specified number of events
     $events = tribe_get_events( array(
-        'posts_per_page' => intval( $atts['events'] ), // Use the 'events' attribute value
+        'posts_per_page' => intval( $atts['events'] ),
         'start_date'     => current_time( 'Y-m-d H:i:s' ),
     ) );
 
     if ( empty( $events ) ) {
-        return 'No upcoming events.';
+        return '<p class="elementor-event-no-events">No upcoming events.</p>';
     }
 
     ob_start(); ?>
-
-    <div class="custom-events-list">
+    
+    <section class="elementor-event-list">
         <?php foreach ( $events as $event ) : ?>
-            <div class="event">
-                <h3 class="event-title"><?php echo esc_html( $event->post_title ); ?></h3>
-                <p class="event-date"><?php echo esc_html( tribe_get_start_date( $event, false, 'F j, Y' ) ); ?></p>
-                <p class="event-description"><?php echo esc_html( wp_trim_words( $event->post_content, 15 ) ); ?></p>
-            </div>
+            <article class="elementor-event">
+                <header class="elementor-event-header">
+                    <h3 class="elementor-event-title"><?php echo esc_html( $event->post_title ); ?></h3>
+                    <p class="elementor-event-date"><?php echo esc_html( tribe_get_start_date( $event, false, 'F j, Y' ) ); ?></p>
+                </header>
+                <div class="elementor-event-content">
+                    <p class="elementor-event-description"><?php echo esc_html( wp_trim_words( $event->post_content, 15 ) ); ?></p>
+                </div>
+            </article>
         <?php endforeach; ?>
-    </div>
+    </section>
 
     <?php return ob_get_clean();
 }
